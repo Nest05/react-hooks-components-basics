bfab4f6177cae93ad5ae8374ca758c2c782fcf08
Nestor Masinde <nestormasinde@gmail.com>
2024-01-30 14:26:11 +0300

    Components Reviewed

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+  return <div>Naturally, I agree with this article. <br/> Yes, and I, Nestor, who was here as well also agree. </div>;
 }
 
 export default Comment;
